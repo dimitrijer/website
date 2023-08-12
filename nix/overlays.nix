9709ba32eb2e5e1fe49a5209c8e5216845fce9a8
Dimitrije Radojević <me@dimitrije.website>
2023-08-12 11:13:02 +0100

    Upgrade to 23.05 of nixpkgs and latest nixfiles.

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,11 +1,4 @@
 self: super: {
-  # Make ghc8107 the default package set for Haskell.
-  haskellPackages = super.haskell.packages.ghc902.override {
-    overrides = self: super: { };
-  };
-  haskell-language-server = super.haskell-language-server.override {
-    supportedGhcVersions = [ "902" ];
-  };
   # Override ghc with specific version. Add dependencies so they show up in the
   # shell -- they don't need to explicitly be specified for the site, as deps
   # are explicitly specified in the derivation.
